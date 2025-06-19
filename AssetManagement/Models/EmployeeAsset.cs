9d9ae6301350e4e0fca2b5f46fb06da38f0b18f6
Santo <santo2004off@gmail.com>
2025-06-19 11:13:10 +0530

    Add project files.

---
+++ AssetManagement/Models/EmployeeAsset.cs
@@ -0,0 +1,14 @@
+﻿namespace AssetManagement.Models
+{
+    public class EmployeeAsset
+    {
+        public int AllocationId { get; set; }
+        public int? UserId { get; set; }
+        public int? AssetId { get; set; }
+        public DateTime? AllocationDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public string? Status { get; set; }
+        public virtual Asset? Asset { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
